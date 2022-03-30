0803c82f02a2fb18c1385733798785d1050afbb0
chaerin99 <cofls990111@naver.com>
2022-03-30 15:37:48 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+// import
+// webpack사용이유: 성능상승, 낭비x, 
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+// export
+module.exports = {
+    //파일을 읽어들이기 시작하는 진입점 설정
+    entry: './js/main.js',
+
+    // 결과물(번들)을 반환하는 설정
+    output: {
+        // path: path.resolve(__dirname, 'dist'),
+        // filename: 'main.js',
+        clean: true 
+    },
+
+    // 번들링 후 결과물의 처리 방식 등 다양한 플러그인들을 설정
+    plugins: [
+        new HtmlPlugin({
+            template: './index.html'
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'static' }
+            ]
+        })
+    ],
+
+    devServer: {
+        host: 'localhost'
+    },
+    module: {
+        rules: [
+          {
+            test: /\.s?css$/,
+            use: [
+              // Creates `style` nodes from JS strings
+              "style-loader",
+              // Translates CSS into CommonJS
+              "css-loader",
+              "postcss-loader",
+              // Compiles Sass to CSS
+              "sass-loader",
+            ],
+          },          
+          {
+            test: /\.js$/ ,
+            use: [
+                'babel-loader'
+            ]
+           },
+        ],
+      },
+}
+
